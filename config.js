a70a65171732374fd5bdabdac3a6b763ae6c05d8
norbertdurcansky <norbert@cinnamon.video>
2020-06-01 18:50:17 +0200

    CONFIG DEMO

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    CLIENT_ID: "031a4380-1905-46ba-84c6-f99e40a24506",
-    CLIENT_SECRET: "ZOZ0zkRdc5XIdCqbRcvBjPNL+DFZf0lGVvkvd/GlqJA=",
+    CLIENT_ID: "835e576c-600e-4348-bc91-9051150ddc4b",
+    CLIENT_SECRET: "vuLQuc4Xtxy8va7EDspIXrIsErrevk4o3ZYupTYerpA=",
     VANILLA_API_URL: "https://api.vanilla.so/graphql"
 }
