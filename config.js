ff370ffeb7d98f7c0fdd11f569e6d97b6988a85e
norbertdurcansky <norbert@cinnamon.video>
2020-06-02 10:18:40 +0200

    Fix URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     CLIENT_ID: "835e576c-600e-4348-bc91-9051150ddc4b",
     CLIENT_SECRET: "vuLQuc4Xtxy8va7EDspIXrIsErrevk4o3ZYupTYerpA=",
-    VANILLA_API_URL: "https://api.vanilla.so/graphql"
+    VANILLA_API_URL: "https://wm.vanilla.so/graphql"
 }
